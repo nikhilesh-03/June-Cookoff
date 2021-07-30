f499229377ef6cee7513fd26274db57b34f4abec
Nikhilesh Vardhan Gupta <78022273+nikhilesh-03@users.noreply.github.com>
2021-07-30 19:40:07 +0530

    Add files via upload

---
+++ ab.py
@@ -0,0 +1,19 @@
+#Question 4
+for _ in range(int(input())):
+    n=int(input())
+    li=list(map(int,input( ).split( )))
+    even=[]
+    odd=[]
+    for i in range(n):
+        if li[i]%2==0:
+            even.append(li[i])
+        else:
+            odd.append(li[i])
+    even.sort()
+    odd.sort(reverse=True)
+    pi=[]
+    for t in range(len(even)):
+        pi.append(even[t])
+    for j in range(len(odd)):
+        pi.append(odd[j])
+    print(*pi)
