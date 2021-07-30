c58ad759d418d27501cd56d22995e71f340b98a7
Nikhilesh Vardhan Gupta <78022273+nikhilesh-03@users.noreply.github.com>
2021-07-30 19:41:17 +0530

    Update HTML TAGS.py

--- HTML TAGS.py
+++ HTML TAGS.py
@@ -1,4 +1,4 @@
-#Quesion3
+#Quesion3(JUNE LUNCHTIME)
 for t in range(int(input())):
     a=list(input())
     li=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
